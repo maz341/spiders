a6ebc946468beece30c991b3b5d35a27e990d02b
Muhammad Maaz Kamal <34510708+maz341@users.noreply.github.com>
2022-06-13 17:45:16 +0500

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+<script>
+// {/* if ('serviceWorker' in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("firebase-messaging-sw.js");
+  });
+
+  window.addEventListener('flutter-first-frame', function () {
+    navigator.serviceWorker.register('flutter_service_worker.js');
+  });                                                                                                                                                                                                                                                                                           
+// } */}
+
+</script>               
